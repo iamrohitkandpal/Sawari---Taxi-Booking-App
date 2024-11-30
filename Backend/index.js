89ce21e17165afd8197441b9b027e645c05b40c7
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-11-30 23:57:37 +0530

    Some more changes in the project

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,11 @@
-import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
+import express from 'express';
+import cors from 'cors';
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Backend Working');
