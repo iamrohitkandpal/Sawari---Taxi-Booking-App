db8f67051a512dfb972a0add5d9f36e386ee6157
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-12-08 23:55:53 +0530

    Adding Cookie parser for cookie based token authentication

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ import express, { urlencoded } from 'express';
 import cors from 'cors';
 import connection from './db/db.js';
 import userRoutes from "./routes/user.routes.js";
+import cookieParser from 'cookie-parser';
 
 const app = express();
 dotenv.config();
@@ -11,6 +12,7 @@ connection();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Backend Working');
