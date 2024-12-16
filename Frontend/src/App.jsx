d67ae8027253d30fab4b98f0971fce27ff526b3f
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-12-16 23:59:31 +0530

    Did some configuration changes for host access and setup the App,jsx file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import UserLogin from './pages/UserLogin';
+import UserSignUp from './pages/UserSignUp';
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainSignUp from "./pages/CaptainSignUp";
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignUp />} />
+        <Route path="/cap-login" element={<CaptainLogin />} />
+        <Route path="/cap-signup" element={<CaptainSignUp />} />
+      </Routes>
     </div>
   )
 }
