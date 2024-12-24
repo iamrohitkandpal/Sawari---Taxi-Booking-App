cdd3df33cf1c8aec7e83293bfc96210abb419bc1
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-12-24 23:56:18 +0530

    Integrating the signup page with backend using Axios package

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,28 +1,9 @@
-import React from "react";
-import { home, logo } from "../assets";
-import { Link } from 'react-router-dom';
+import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <div
-        className="h-screen pt-5 w-full justify-between flex flex-col"
-        style={{
-          backgroundImage: `url(${home})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center top",
-        }}
-      >
-        <img className="w-28 ml-5" src={logo} alt="App Logo" />
-        <div className="bg-white py-2 px-3">
-          <h2 className="text-2xl font-bold">Get Started with Sawaari</h2>
-          <Link to="/login" className="flex items-center justify-center w-full text-white bg-black py-3 rounded-md my-7">
-            Continue
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <div>Home</div>
+  )
+}
 
-export default Home;
+export default Home
--- Frontend/src/pages/UserSignUp.jsx
+++ Frontend/src/pages/UserSignUp.jsx
@@ -1,24 +1,38 @@
 import React, { useState } from "react";
 import { logo } from "../assets";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { UserContext } from "./../context/UserContext";
 
 const UserSignUp = () => {
   const [email, setEmail] = useState("");
   const [fullname, setFullname] = useState({ firstname: "", lastname: "" });
   const [password, setPassword] = useState("");
-
   const [userData, setUserData] = useState({});
+  
+  const navigate = useNavigate();
 
-  const submitHandler = (e) => {
+  const { user, setUser } = React.useContext(UserContext);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setUserData({
-      email: email,
-      password: password,
-      fullname: {
-        firstname: fullname.firstname,
-        lastname: fullname.lastname,
-      },
-    });
+    const newUser = {
+      email,
+      fullname,
+      password,
+    };
+
+    const response = await axios.post(
+      `${import.meta.env.VITE_BASE_URL}/user/register`,
+      newUser
+    );
+
+    if (response.status === 201) {
+      const data = response.data;
+      setUser(data).user;
+
+      navigate("/home");
+    }
 
     setEmail("");
     setFullname({ firstname: "", lastname: "" });
@@ -39,7 +53,9 @@ const UserSignUp = () => {
               type="name"
               required
               value={fullname.firstname}
-              onChange={(e) => setFullname({ ...fullname, firstname: e.target.value })}
+              onChange={(e) =>
+                setFullname({ ...fullname, firstname: e.target.value })
+              }
               placeholder="First Name"
               className="bg-[#eeeeee] mb-4 rounded-md px-4 py-3 w-1/2 border text-sm placeholder:text-sm"
             />
@@ -47,7 +63,9 @@ const UserSignUp = () => {
               type="name"
               required
               value={fullname.lastname}
-              onChange={(e) => setFullname({ ...fullname, lastname: e.target.value })}
+              onChange={(e) =>
+                setFullname({ ...fullname, lastname: e.target.value })
+              }
               placeholder="Last Name"
               className="bg-[#eeeeee] mb-4 rounded-md px-4 py-3 w-1/2 border text-sm placeholder:text-sm"
             />
@@ -72,18 +90,23 @@ const UserSignUp = () => {
           />
 
           <button className="bg-black text-white mb-2 rounded-md px-4 py-[0.7rem] w-full text-sm">
-            Register
+            Create Account
           </button>
 
           <p className="text-sm text-center">
             Done Already?
-            <Link to='/login' className="text-blue-500 font-semibold"> Sign In</Link>
+            <Link to="/login" className="text-blue-500 font-semibold">
+              {" "}
+              Sign In
+            </Link>
           </p>
         </form>
       </div>
       <div className="px-2">
         <p className="text-[11px] text-zinc-600 text-center leading-tight">
-        This site is protected by reCapcha and the <span className="underline">Google Privacy Policy</span> and <span className="underline">Terms of Service</span> apply.
+          This site is protected by reCapcha and the{" "}
+          <span className="underline">Google Privacy Policy</span> and{" "}
+          <span className="underline">Terms of Service</span> apply.
         </p>
       </div>
     </div>
