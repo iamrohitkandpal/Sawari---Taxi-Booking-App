d0422f39dda6d70da9e21620db5a1b35d8c8d66c
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-02-18 23:50:43 +0530

    Enhance UserProtectedWrapper to load user profile and handle token validation

--- Frontend/src/pages/UserProtectedWrapper.jsx
+++ Frontend/src/pages/UserProtectedWrapper.jsx
@@ -1,22 +1,42 @@
 import React, { useContext, useEffect } from "react";
 import { UserDataContext } from "../context/UserContext";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const UserProtectedWrapper = ({children}) => {
-    const token = localStorage.getItem('token');
-    const navigate = useNavigate();
+const UserProtectedWrapper = ({ children }) => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserDataContext);
+  const [isLoading, setIsLoading] = React.useState(true);
 
-    useEffect(() => {
-        if(!token){
-            navigate('/login');
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+    axios
+      .get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setUser(response.data);
+          setIsLoading(false);
         }
-    }, [token, navigate]);
+      })
+      .catch((error) => {
+        console.log(error);
+        localStorage.removeItem("token");
+        navigate("/login");
+      });
+  }, [token, navigate, setUser]);
 
-    return (
-        <div>
-            {children}
-        </div>
-    )
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{children}</div>;
 };
 
 export default UserProtectedWrapper;
