52a1e85dd4dc88bf3d95adb126deed065212c34f
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-03-01 23:58:31 +0530

    Add RidePopup component and integrate it into CapHome for new ride notifications

--- Frontend/src/pages/CapHome.jsx
+++ Frontend/src/pages/CapHome.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import CaptainDetails from "../components/CaptainDetails";
 import { logo } from "../assets/index";
+import RidePopUp from "./../components/RidePopup";
+import { useGSAP } from '@gsap/react';
+import gsap from "gsap";
 
 const CapHome = () => {
+  const [ride, setRide] = useState(null);
+  const [ridePopupPanel, setRidePopupPanel] = useState(true);
+  const [confirmRidePopupPanel, setConfirmRidePopupPanel] = useState(false);
+
+  const ridePopupPanelRef = useRef(null);
+  const confirmRidePopupPanelRef = useRef(null);
+
+  useGSAP(function () {
+    gsap.to(ridePopupPanelRef.current, {
+      transform: ridePopupPanel ? "translateY(0)" : "translateY(100%)",
+    });
+  })
+
   return (
     <div className="h-screen">
       <div className="fixed p-6 top-0 flex items-center justify-between w-screen">
@@ -15,7 +31,7 @@ const CapHome = () => {
           <i className="text-lg font-medium ri-logout-box-r-line"></i>
         </Link>
       </div>
-      <div className="h-3/5">
+      <div className="h-3/5"> 
         <img
           className="h-full w-full object-cover"
           src="https://miro.medium.com/v2/resize:fit:1280/0*gwMx05pqII5hbfmX.gif"
@@ -24,6 +40,14 @@ const CapHome = () => {
       </div>
       <div className="h-2/5 p-4">
         <CaptainDetails />
+      </div>
+      <div ref={ridePopupPanelRef} className="fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12">
+        <RidePopUp
+          ride={ride}
+          setRidePopupPanel={setRidePopupPanel}
+          setConfirmRidePopupPanel={setConfirmRidePopupPanel}
+          // confirmRide={confirmRide}
+        />
       </div>
     </div>
   );
