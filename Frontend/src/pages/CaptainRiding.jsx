e9adff3ff9c375f1ef88a70346ae82a5dc1ec481
Rohit Kandpal <iamrohitkandpal@gmail.com>
2025-03-02 23:54:08 +0530

    Add FinishRide and LiveTracking components; implement CaptainRiding page layout

---
+++ Frontend/src/pages/CaptainRiding.jsx
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import FinishRide from '../components/FinishRide'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import {logo} from '../assets/index'
+import LiveTracking from '../components/LiveTracking'
+
+const CaptainRiding = () => {
+
+    const [ finishRidePanel, setFinishRidePanel ] = useState(false)
+    const finishRidePanelRef = useRef(null)
+    const location = useLocation()
+    const rideData = location.state?.ride
+
+
+
+    useGSAP(function () {
+        gsap.to(finishRidePanelRef.current, {
+            transform: finishRidePanel ? 'translateY(0)' : 'translateY(100%)',
+            duration: 0.75,
+            ease: 'power4.inOut'
+        })
+    }, [ finishRidePanel ])
+
+
+    return (
+        <div className='h-screen relative flex flex-col justify-end'>
+
+            <div className='fixed p-4 top-0 flex items-center justify-between w-screen'>
+                <img className="w-28 mb-8" src={logo} alt="" />
+                <Link to='/captain-home' className=' h-12 w-12 mb-8 bg-white flex items-center justify-center rounded-full'>
+                    <i className="text-lg font-medium ri-logout-box-r-line"></i>
+                </Link>
+            </div>
+
+            <div className='h-1/5 p-6 flex items-center justify-between relative bg-gray-400 pt-10'
+                onClick={() => {
+                    setFinishRidePanel(true)
+                }}
+            >
+                <h5 className='p-1 text-center w-[90%] absolute top-0' onClick={() => {
+
+                }}><i className="text-3xl text-gray-800 ri-arrow-up-wide-line"></i></h5>
+                <h4 className='text-xl font-semibold'>{'4 KM away'}</h4>
+                <button className=' bg-black text-white font-semibold p-3 px-10 rounded-lg'>Complete Ride</button>
+            </div>
+            <div ref={finishRidePanelRef} className='fixed w-full z-[500] bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+                <FinishRide
+                    ride={rideData}
+                    setFinishRidePanel={setFinishRidePanel} />
+            </div>
+
+            <div className='h-screen fixed w-screen top-0 z-[-1]'>
+                <LiveTracking />
+            </div>
+
+        </div>
+    )
+}
+
+export default CaptainRiding
